260a92e62e1f7d61613135db66240c03b5cc9b9d
Chris Skilton <chriss@brandwatch.com>
2017-09-07 18:21:04 +0100

    add config for babel and webpack to build the sweet codes

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        app: [
+            path.resolve(__dirname, 'src/app.js')
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            loader: 'css-loader',
+            exclude: /node_modules/
+        }]
+    }
+};
