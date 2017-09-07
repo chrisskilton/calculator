c5c7e2265b48a71db1308720666af0eff33f7fd1
Chris Skilton <chriss@brandwatch.com>
2017-09-07 18:31:43 +0100

    Reuse the Operator code to make the ExecutorPad, which handles the equals and clear functions

---
+++ src/components/ExecutorPad.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Operator from './Operator';
+import {
+    equals,
+    clear
+} from '../actions/operators';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+const ExecutorPad = ({equals, clear}) => (
+    <div className="executorPad">
+        <Operator label="equals" key="equals" type="=" onClick={() => equals} />
+        <Operator label="clear" key="clear" type="C" onClick={() => clear} />
+    </div>
+);
+
+export default connect(null, dispatch => bindActionCreators({
+    equals,
+    clear
+}, dispatch))(ExecutorPad);
